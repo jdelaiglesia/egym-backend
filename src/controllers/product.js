061882e0a46d262ad8bcd802ad840e7e71859e93
Dafting <joaquindlaiglesia@gmail.com>
2024-04-11 00:08:35 -0300

    product model created

---
+++ src/controllers/product.js
@@ -0,0 +1,16 @@
+const Product = require("../models/Product");
+
+const getProduct = async (req, res) => {};
+
+const createProduct = async (req, res) => {};
+
+const updateProduct = async (req, res) => {};
+
+const deleteProduct = async (req, res) => {};
+
+module.exports = {
+  getProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+};
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,8 +2,25 @@ const { Router } = require("express");
 
 const router = Router();
 
-const hellowWorld = require("../controllers/helloWorld")
+const helloWorld = require("../controllers/helloWorld");
+const {
+  getProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} = require("../controllers/product");
 
-router.get("/helloworld", hellowWorld);
+//! GET
+router.get("/helloworld", helloWorld);
+router.get("/product", getProduct);
+
+//! POST
+router.post("/product", createProduct);
+
+//! PUT
+router.put("/product/:id", updateProduct);
+
+//! DELETE
+router.delete("/product/:id", deleteProduct);
 
 module.exports = router;
