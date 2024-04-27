e9662f81a6eff665fb5ccb09f201f6f8dbf2b2aa
Joaquin de la Iglesia <joaquindlaiglesia@gmail.com>
2024-04-27 17:39:54 -0300

    comenzando incidencia PF-76-model-schema-y-crud-de-ventas

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -8,8 +8,8 @@ const {
   createDiscountCoupon,
   getDiscountCoupon,
   updateDiscountCoupon,
-  deleteDiscountCoupon
-} = require("../controllers/discountCoupon")
+  deleteDiscountCoupon,
+} = require("../controllers/discountCoupon");
 
 const {
   getProduct,
@@ -19,7 +19,7 @@ const {
   getAllProducts,
   getNameProduct,
   getCategoryProducts,
-  getFilteredProducts
+  getFilteredProducts,
 } = require("../controllers/product");
 
 const {
@@ -30,58 +30,61 @@ const {
   deleteUser,
   userLogin,
   putMember,
-  putRank
-} = require('../controllers/user')
+  putRank,
+} = require("../controllers/user");
 
 const {
   getCategories,
   getCategory,
   postCategory,
   putCategory,
-  deleteCategory
-} = require('../controllers/category')
+  deleteCategory,
+} = require("../controllers/category");
 
 const {
   getCommentsProduct,
   postComment,
-  deleteComment
-} = require('../controllers/comment')
+  deleteComment,
+} = require("../controllers/comment");
+
+const { createSale } = require("../controllers/sale");
 
 //! GET
 router.get("/helloworld", hellowWorld);
 router.get("/product/:id", getProduct);
-router.get("/products", getAllProducts)
-router.get("/products/category/:id", getCategoryProducts)
-router.get("/products/filters", getFilteredProducts)
-router.get("/product/name/:name", getNameProduct)
-router.get("/coupon/:name", getDiscountCoupon)
-router.get("/users", getUsers)
-router.get("/user/:email", getUserByEmail)
-router.get("/categories", getCategories)
-router.get("/category/:name", getCategory)
-router.get("/comments/:id", getCommentsProduct)
+router.get("/products", getAllProducts);
+router.get("/products/category/:id", getCategoryProducts);
+router.get("/products/filters", getFilteredProducts);
+router.get("/product/name/:name", getNameProduct);
+router.get("/coupon/:name", getDiscountCoupon);
+router.get("/users", getUsers);
+router.get("/user/:email", getUserByEmail);
+router.get("/categories", getCategories);
+router.get("/category/:name", getCategory);
+router.get("/comments/:id", getCommentsProduct);
 
 //! POST
 router.post("/product", createProduct);
-router.post("/coupon", createDiscountCoupon)
-router.post("/user", postUser)
-router.post("/user/login", userLogin)
-router.post("/category", postCategory)
-router.post("/comment", postComment)
+router.post("/coupon", createDiscountCoupon);
+router.post("/user", postUser);
+router.post("/user/login", userLogin);
+router.post("/category", postCategory);
+router.post("/comment", postComment);
+router.post("/sale", createSale);
 
 //! PUT
 router.put("/product/:id", updateProduct);
-router.put("/coupon", updateDiscountCoupon)
-router.put("/user/:id", putUser)
-router.put("/user/member/:id", putMember)
-router.put("/user/rank/:id", putRank)
-router.put("/category/:id", putCategory)
+router.put("/coupon", updateDiscountCoupon);
+router.put("/user/:id", putUser);
+router.put("/user/member/:id", putMember);
+router.put("/user/rank/:id", putRank);
+router.put("/category/:id", putCategory);
 
 //! DELETE
 router.delete("/product/:id", deleteProduct);
-router.delete("/user/:id", deleteUser)
-router.delete("/category/:id", deleteCategory)
-router.delete("/comment/:id", deleteComment)
-router.delete("/coupon/:id", deleteDiscountCoupon)
+router.delete("/user/:id", deleteUser);
+router.delete("/category/:id", deleteCategory);
+router.delete("/comment/:id", deleteComment);
+router.delete("/coupon/:id", deleteDiscountCoupon);
 
 module.exports = router;
